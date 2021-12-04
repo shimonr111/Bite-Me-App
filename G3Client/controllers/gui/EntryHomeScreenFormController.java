d2a0da5d6fc2f53d1d5de37a909a216a050107d8
Ori Malka <85791232+orimalka@users.noreply.github.com>
2021-12-04 09:53:01 +0200

    Update EntryHomeScreenFormController.java

--- G3Client/controllers/gui/EntryHomeScreenFormController.java
+++ G3Client/controllers/gui/EntryHomeScreenFormController.java
@@ -1,5 +1,5 @@
-
 package gui;
+
 import bitemeclient.PopUpMessages;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,7 +9,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
 
 /**
  * @author Shimon Rubin
@@ -25,22 +24,26 @@ public class EntryHomeScreenFormController{
 	 * Class members description:
 	 */
 	
-	/**This button 
-	 * Enter to the Login 
-	 * screen */
+	/**
+	 * This button enter to
+	 * the Login screen
+	 */
 	@FXML
 	private Button btnEnter = null;
 	
-	/**This button 
-	 * exit from the App 
-	 * immediately */
+	/**
+	 * This button exits
+	 * the application
+	 */
 	@FXML
 	private Button btnExit = null;
 	
-	/**This button pop 
+	/**
+	 * This button pop 
 	 * up message which show 
 	 * to us info about the 
-	 * current page */
+	 * current page 
+	 */
 	@FXML
 	private Button btnHelp = null;
 	
@@ -50,9 +53,11 @@ public class EntryHomeScreenFormController{
 	}
 
 
-	/**Using enter button hide 
+	/**
+	 * Using enter button hide 
 	 * window and go login 
-	 * screen */
+	 * screen 
+	 */
 	public void getEnterBtn(ActionEvent event) throws Exception {
 		((Node) event.getSource()).getScene().getWindow().hide(); // hiding primary window
 		Stage primaryStage = new Stage();
@@ -65,16 +70,20 @@ public class EntryHomeScreenFormController{
 	}
 
 	
-	/**Using help button in order 
+	/**
+	 * Using help button in order 
 	 * to show instructions for the 
-	 * current screen */
+	 * current screen 
+	 */
 	public void getHelpBtn(ActionEvent event) throws Exception {
 		PopUpMessages.helpMessage("Welcome to BiteMe App! press Enter to login your user.");
 	}
 	
-	/**Using exit button 
+	/**
+	 * Using exit button 
 	 * in order to exit 
-	 * from the App */
+	 * from the App 
+	 */
 	public void getExitBtn(ActionEvent event) throws Exception {
 		System.exit(0);
 	}
