25ac815340a076f63fc86697644e5352784ff37f
Ori Malka <85791232+orimalka@users.noreply.github.com>
2021-12-05 10:28:05 +0200

    Update LoginQueries.java

--- G3Server/src/query/LoginQueries.java
+++ G3Server/src/query/LoginQueries.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-
 import communication.Message;
 import users.Branch;
 import users.BudgetType;
