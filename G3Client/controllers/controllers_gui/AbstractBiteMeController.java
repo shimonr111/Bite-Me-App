69a4c483f4e5e1a0904d3cffe5b0ea13642cdd1f
Ori Malka <85791232+orimalka@users.noreply.github.com>
2021-12-08 09:09:30 +0200

    Update AbstractBiteMeController.java

--- G3Client/controllers/controllers_gui/AbstractBiteMeController.java
+++ G3Client/controllers/controllers_gui/AbstractBiteMeController.java
@@ -1,28 +1,31 @@
 package controllers_gui;
 
 import java.util.ArrayList;
-
 import bitemeclient.BiteMeClientUI;
 import communication.Message;
 import users.User;
 
 /**
  * @author Lior, Guzovsky.
+ * @author Mousa, Srour.
+ *
  * Class description: 
  * 
  * This Abstract class will 
  * be key designed for all controllers
  * 
- * @version 03/12/2021
+ * @version 08/12/2021
  */
 public abstract class AbstractBiteMeController {
 	/**
 	 * Class members description:
 	 */
 	
-	// This is the connected user, every client has only 1 or 0 connected users 
-	// every time an user logged in we set the connectedUser as the connected user
-	// after the logout the connectedUser will be null.
+	/**
+	 * This is the connected user, every client has only 1 or 0 connected users 
+	 * every time an user logged in we set the connectedUser as the connected user
+	 * after the logout the connectedUser will be null.
+	 */
 	public static User connectedUser;
 	
 	/**
