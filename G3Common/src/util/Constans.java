0671013298c84d8262b5bb4d29962ecdda049c2c
unknown <mosa.srour.s9@gmail.com>
2021-12-27 22:35:02 +0200

    update customer balance and actual supply time

--- G3Common/src/util/Constans.java
+++ G3Common/src/util/Constans.java
@@ -9,7 +9,10 @@ public class Constans {
 	public static final double REGULAR_TAKE_AWAY_SUPPLY_TIME_IN_MINUTES = 60.0;
 	public static final double PRE_ORDER_TAKE_AWAY_SUPPLY_TIME_IN_MINUTES = 20.0;
 	public static final double PRE_ORDER_DISCOUNT = 0.1;
+	public static final long ONE_HOUR_IN_MILLESECONDS = 3600000;
+	public static final double FIFTY_PERCENT = 0.5;
 	public static final long TEN_MINUTES_IN_MILLESECONDS = 600000;
+	public static final long TWENTY_MINUTES_IN_MILLESECONDS = 1200000;
 	public static final double DAYS_IN_MONTH = 31.0;
 	public static final double WEEKS_IN_MONTH = 4.0;
 }
